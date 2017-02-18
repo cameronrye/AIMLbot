29513c37bedb2749d16faed37c85fb381b5ada64
Cameron Rye <c@meron.io>
2017-02-18 11:47:32 -0500

    Testing
    Additional Tests

--- test/aiml.js
+++ test/aiml.js
@@ -3,7 +3,12 @@
 const test = require('ava')
 const AIML = require('../lib/aiml')
 
-let bot = new AIML()
+let name = 'zia'
+let bot = new AIML({ 'name': name })
+
+test('Bot Name', t => {
+  t.is(bot.name(), name)
+})
 
 test('Bot Termination', t => {
   t.is(bot.quit(), 'bye bye')
